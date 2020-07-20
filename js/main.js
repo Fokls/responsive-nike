6657b8ea00370605e3d686fb78d8323022f45cba
Fokls <foklacu@gmail.com>
2020-07-20 23:59:12 +0300

    slider is finished

--- js/main.js
+++ js/main.js
@@ -13,5 +13,17 @@ $(window).scroll(function(){
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n){
-    showslides(slideIndex += n);
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('myslides');
+    if(n>slides.length){
+        slideIndex=1
+    }
+    if (n<1){slideIndex=slides.length}
+    for (i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';}
+        slides[slideIndex-1].style.display = 'block';
 }
